8671a1218264d5d4a0f2a08ea3d0c781ff5db4ac
wonijjang0915 <55573431+wonijjang0915@users.noreply.github.com>
2019-10-08 15:12:27 +0900

    05_04

--- main.c
+++ main.c
@@ -4,23 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-    int number;
+    int i=0, meter;
     
-    printf("input a number :");
-    scanf("%d", &number);
-    
-    if ( number > 0 )
+    while (i<3)
     {
-	  printf("positive\n");
-    }
-    else if ( number < 0 )
-    {
-      printf("negative\n");
-    }
-    else
-    {
-      printf("zero\n");
-    }
+    	meter = i*1609;
+    	printf("%d 마일은 %d 미터입니다.\n", i, meter);
+    	i++;
+	}
 	  
 	return 0;
 }
