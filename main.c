d9cf0d632f5ea927e61742e8ea9f5e59c14e6e59
wonijjang0915 <55573431+wonijjang0915@users.noreply.github.com>
2019-10-08 16:19:59 +0900

    05_07

--- main.c
+++ main.c
@@ -4,20 +4,26 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-    int num = 0;
-    char c;
+    int answer = 55;
+    int input;
+    int cnt = 0;
     
-    printf("input a string: ");
-    
-    while ((c = getchar()) !='\n')
+    printf("Guess a number : ");
+    do
     {
-    	if (c <= '9' && c >= '0')
+    	scanf("%d", &input);
+    	if (input < answer)
     	{
-    		num++;
-    		
+    		printf("low!\n");
 		}
-	}
-    printf("the number of digits is %i\n", num);
+		else if (input > answer)
+		{
+			printf("high!\n");
+		}
+		cnt++;
+	} 
+	while ( input !=answer );
+	printf("Congratulation, trial : %i\n", cnt);
    
 	return 0;
 }
