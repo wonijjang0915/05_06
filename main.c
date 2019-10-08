9271cdcb047f29b3b4f06561c81725e44bc546f0
wonijjang0915 <55573431+wonijjang0915@users.noreply.github.com>
2019-10-08 16:02:47 +0900

    05_06

--- main.c
+++ main.c
@@ -4,9 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-    int i;
-    for ( i=0; i<10; i++)
-      printf("Hello World!\n");
-  	  
+    int num = 0;
+    char c;
+    
+    printf("input a string: ");
+    
+    while ((c = getchar()) !='\n')
+    {
+    	if (c <= '9' && c >= '0')
+    	{
+    		num++;
+    		
+		}
+	}
+    printf("the number of digits is %i\n", num);
+   
 	return 0;
 }
